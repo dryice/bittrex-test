c628277a8f93392429681d5fcb89425c7f8924f1
Dryice Liu <dryiceliu@gmail.com>
2017-09-29 07:59:08 +0800

    calculate how many we'll get

--- buyer.py
+++ buyer.py
@@ -28,11 +28,29 @@ def get_orderbook(currency):
     else:
         print "Something is wrong. Error message from bittrex: {}".format(result['message'])
         sys.exit(1)
-        
+
+def do_calculate(offers, amount):
+    amount_left = amount
+    total_get = 0
+
+    for offer in offers:
+        if amount_left > offer['Quantity']:
+            total_get += offer['Quantity'] * offer['Rate']
+            amount_left -= offer['Quantity']
+        else:
+            total_get += amount_left * offer['Rate']
+            amount_left = 0
+
+    return total_get, amount_left
 
 if __name__ == "__main__":
     parser = command_parser()
     args = parser.parse_args()
 
     offers = get_orderbook(args.currency)
-    print offers[:20]
+    total_get, amount_left = do_calculate(offers, args.amount)
+
+    if amount_left:
+        print "We'll get {} BTC, and still have {} {} left".format(total_get, amount_left, args.currency)
+    else:
+        print "We'll get {} BTC".format(total_get)
