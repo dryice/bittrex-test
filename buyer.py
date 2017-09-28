aa37bfd5a1214c8c8a14cbddaff5dde92f4bdcf0
Dryice Liu <dryiceliu@gmail.com>
2017-09-29 07:48:16 +0800

    handle errors from bittrex

--- buyer.py
+++ buyer.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import sys
 
 import bittrex
 
@@ -19,11 +20,15 @@ def command_parser():
 def get_orderbook(currency):
     my_bittrex = bittrex.Bittrex(API_KEY, API_KEY_SECRET)
     market = "BTC-{}".format(currency)
-    offers = my_bittrex.get_orderbook(market, bittrex.BUY_ORDERBOOK)['result']
 
-    return offers
-    
+    result = my_bittrex.get_orderbook(market, bittrex.BUY_ORDERBOOK)
 
+    if result['success']:
+        return result['result']
+    else:
+        print "Something is wrong. Error message from bittrex: {}".format(result['message'])
+        sys.exit(1)
+        
 
 if __name__ == "__main__":
     parser = command_parser()
